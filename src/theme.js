8ffa5982847673bc4921ada19a70115c361d1846
changha <mentenseoul@gmail.com>
2025-04-05 11:42:33 +0900

    init project

---
+++ src/theme.js
@@ -0,0 +1,33 @@
+// src/theme.js
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#42E6B5',
+    },
+    background: {
+      default: '#12121a',
+      paper: '#1e1f2d',
+      input: '#2c2c3a',
+      inputDisabled: '#424254',
+    },
+    text: {
+      primary: '#ffffff',
+      secondary: '#aaa',
+      disabled: '#7B7A8E',
+      sub: '#B7B7C9',
+    },
+    border: {
+      main: '#3c3d4e'
+    },
+    custom: {
+      menuBg: '#2b2c3c',
+    }
+  },
+  shape: {
+    borderRadius: 10,
+  },
+});
+
+export default theme;
